6f62f1a95d066f3d2feff0171f04e5fb1256897a
f <105430794+404BNFound@users.noreply.github.com>
2022-05-15 09:45:36 +0300

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,39 +1,6 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Grid, Paper, Snackbar, Button, Backdrop, CircularProgress, Alert, AlertTitle } from '@mui/material'
-import AnimatedNumber from 'react-animated-number'
-import './assets/css/Test.css'
 const Test = () => {
-    const [open, setOpen] = useState(true);
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleClick = () => {
-        setOpen(true);
-    };
-    const handleToggle = () => {
-        setOpen(!open);
-    };
-    const [number, setNumber] = useState(0)
-    useEffect(() => {
-        setNumber(4999)
-    }, [])
-
     return (
         <>
-        <Link to={"/choixEndroit"}>Chox endroit test</Link>
-            <div className="content" style={{ textAlign: "center" }}>
-                <AnimatedNumber
-                    value={number}
-                    style={{
-                        fontSize: '4em',
-                    }}
-                    formatValue={n => n.toFixed(0)}
-                    frameStyle={percentage => percentage > 20 && percentage < 80 ? { opacity: 0.5 } : { opacity: 1 }}
-                    duration={2000}
-                >
-                </AnimatedNumber><span style={{ fontSize: '2em' }}>Current investors</span>
-            </div>
         </>
     );
 }
